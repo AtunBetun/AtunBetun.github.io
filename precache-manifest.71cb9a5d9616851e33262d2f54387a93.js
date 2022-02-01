ead552049b4f20e63ea96560023c7848d88cad64
Alberto <beto7299@gmail.com>
2022-02-01 18:52:08 -0500

    Updates

---
+++ precache-manifest.71cb9a5d9616851e33262d2f54387a93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddb4387d39d7505aca6a91e79074dd1",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d743e000fff42f9e636c",
+    "url": "/home/static/css/main.9d79d83d.chunk.css"
+  },
+  {
+    "revision": "17c02a90595572cbc382",
+    "url": "/home/static/js/2.96817ec4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.96817ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d743e000fff42f9e636c",
+    "url": "/home/static/js/main.23486e34.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
